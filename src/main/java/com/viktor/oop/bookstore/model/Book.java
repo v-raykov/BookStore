1c3536a4a63a2ccf465b4fb1045b61bafedc5410
reipo38 <v03a01raik@pm.me>
2025-02-12 16:02:09 +0200

    replaced hardcoded constructors in Book class with lombok annotation

--- src/main/java/com/viktor/oop/bookstore/model/Book.java
+++ src/main/java/com/viktor/oop/bookstore/model/Book.java
@@ -6,7 +6,7 @@ import lombok.*;
 import java.util.UUID;
 
 @Data
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class Book {
     //@Id
     //@GeneratedValue(strategy = GenerationType.AUTO)
@@ -15,15 +15,4 @@ public class Book {
     private String title;
     private String author;
     private int yearPublished;
-
-    public Book(UUID isbn, String title, String author, int yearPublished) {
-        this.isbn = isbn;
-        this.title = title;
-        this.author = author;
-        this.yearPublished = yearPublished;
-    }
-
-    public Book(UUID isbn, BookDto bookDto) {
-        this(UUID.randomUUID(), bookDto.getTitle(), bookDto.getAuthor(), bookDto.getYearPublished());
-    }
 }
